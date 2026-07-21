81dfc12d0938499bd53762d2c109123963f2dea9
RiyaBisht716 <riyabisht716@gmail.com>
2026-07-21 23:35:31 +0530

    Update Firebase configuration

--- Client/src/utils/firebase.js
+++ Client/src/utils/firebase.js
@@ -1,12 +1,12 @@
 import { initializeApp } from "firebase/app";
-import {getAuth, GoogleAuthProvider, signInWithPopup} from "firebase/auth"
+import { getAuth, GoogleAuthProvider, signInWithPopup } from "firebase/auth"
 const firebaseConfig = {
   apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
   authDomain: "shifraai-4da91.firebaseapp.com",
-  projectId: "shifraai",
-  storageBucket: "shifraai.firebasestorage.app",
-  messagingSenderId: "1095099448118",
-  appId: "1:1095099448118:web:6cbcb1b989228c5fc99197"
+  projectId: "shifraai-4da91",
+  storageBucket: "shifraai-4da91.firebasestorage.app",
+  messagingSenderId: "1065375851273",
+  appId: "1:1065375851273:web:8b5f1f958f647a78e7a9d8"
 };
 
 // Initialize Firebase
@@ -26,6 +26,6 @@ provider.setCustomParameters({
 // Google login helper that handles popup auth
 const googleLogin = () => signInWithPopup(auth, provider)
 
-export {auth , provider, googleLogin}
+export { auth, provider, googleLogin }
 
 
